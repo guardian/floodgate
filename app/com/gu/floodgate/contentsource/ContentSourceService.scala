71b3151f9f43eaeb6e8e57372cb7573c4d888b11
Luke Taylor <luke.taylor@guardian.co.uk>
2016-01-27 10:49:08 +0000

    Add ability to initiate reindex via API.

--- app/com/gu/floodgate/contentsource/ContentSourceService.scala
+++ app/com/gu/floodgate/contentsource/ContentSourceService.scala
@@ -1,13 +1,23 @@
 package com.gu.floodgate.contentsource
 
-import com.gu.floodgate.DynamoDBTable
-
+import com.gu.floodgate.{ ContentSourceNotFound, CustomError, DynamoDBTable }
+import org.scalactic.Or
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class ContentSourceService(contentSourceTable: DynamoDBTable[ContentSource]) {
 
   def getContentSources(): Future[Seq[ContentSource]] = contentSourceTable.getAll()
-  def getContentSource(id: String): Future[Option[ContentSource]] = contentSourceTable.getItem(id)
+
+  def getContentSource(id: String): Future[ContentSource Or CustomError] = {
+    contentSourceTable.getItem(id) map { maybeContentSource =>
+      Or.from(
+        option = maybeContentSource,
+        orElse = ContentSourceNotFound(s"A content source with id: $id does not exist.")
+      )
+    }
+  }
+
   def createContentSource(contentSource: ContentSource) = contentSourceTable.saveItem(contentSource)
   def updateContentSource(id: String, contentSource: ContentSource) = contentSourceTable.updateItem(id, contentSource)
   def deleteContentSource(id: String) = contentSourceTable.deleteItem(id)
