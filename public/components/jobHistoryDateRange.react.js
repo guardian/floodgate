2d4924fcbbbee4fc05d058f5837dae1104405c26
Chris Birchall <chris.birchall@guardian.co.uk>
2016-04-19 11:59:47 +0100

    Use moment.js for date formatting

--- public/components/jobHistoryDateRange.react.js
+++ public/components/jobHistoryDateRange.react.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import R from 'ramda';
+import Moment from 'moment';
 
 
 export default class JobHistoryDateRange extends React.Component {
@@ -16,15 +17,19 @@ export default class JobHistoryDateRange extends React.Component {
         return !this.empty(value);
     }
 
+    date(epochMillis) {
+        return Moment(epochMillis).format("YYYY-MM-DD");
+    }
+
     render () {
         if (this.empty(this.props.rangeFrom) && this.empty(this.props.rangeTo)) {
             return <span>All time</span>;
         } else if (this.nonEmpty(this.props.rangeFrom) && this.empty(this.props.rangeTo)) {
-            return <span>{ new Date(this.props.rangeFrom).toISOString().slice(0,10) + " - now" }</span>;
+            return <span>{ this.date(this.props.rangeFrom) + " - now" }</span>;
         } else if (this.empty(this.props.rangeFrom) && this.nonEmpty(this.props.rangeTo)) {
-            return <span>{ "Start of time - " + new Date(this.props.rangeTo).toISOString().slice(0,10) }</span>;
+            return <span>{ "Start of time - " + this.date(this.props.rangeTo) }</span>;
         } else {
-          return <span>{ new Date(this.props.rangeFrom).toISOString().slice(0,10) + " - " + new Date(this.props.rangeTo).toISOString().slice(0,10) }</span>;
+            return <span>{ this.date(this.props.rangeFrom) + " - " + this.date(this.props.rangeTo) }</span>;
         }
     }
 }
