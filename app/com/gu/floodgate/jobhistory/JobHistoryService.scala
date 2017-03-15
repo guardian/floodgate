0649d50cfa26a7bb7734b3863284b694a73f5184
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2017-03-15 16:35:14 +0000

    Fix test

--- app/com/gu/floodgate/jobhistory/JobHistoryService.scala
+++ app/com/gu/floodgate/jobhistory/JobHistoryService.scala
@@ -1,14 +1,18 @@
 package com.gu.floodgate.jobhistory
 
 import com.gu.floodgate.DynamoDBTable
+import com.gu.floodgate.reindex.ReindexStatus
+import com.gu.floodgate.reindex.ReindexStatus._
 import com.gu.scanamo.DynamoFormat
+import com.gu.scanamo.DynamoFormat._
 import org.joda.time.{ DateTimeZone, DateTime }
-
 import scala.concurrent.Future
+import cats.syntax.either._
 
 class JobHistoryService(jobHistoryTable: DynamoDBTable[JobHistory]) {
 
   implicit val dateFormat = DynamoFormat.coercedXmap[DateTime, String, IllegalArgumentException](DateTime.parse(_).withZone(DateTimeZone.UTC))(_.toString)
+  implicit val reindexStatusFormat = DynamoFormat.coercedXmap[ReindexStatus, String, IllegalArgumentException](s => ReindexStatus.fromString(s).getOrElse(Unknown))(ReindexStatus.asString)
 
   def createJobHistory(jobHistory: JobHistory): Unit = jobHistoryTable.saveItem(jobHistory)
   def getJobHistories(): Future[Seq[JobHistory]] = jobHistoryTable.getAll()
