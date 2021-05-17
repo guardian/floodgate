6441f5d5ea59b3a8481d4f39f173eeb6cfbcfc83
Fweddi <freddie.preece@guardian.co.uk>
2021-05-17 12:22:52 +0100

    Remove deprecated dedupe plugin for webpack

--- public/build_config/webpack.prod.conf.js
+++ public/build_config/webpack.prod.conf.js
@@ -18,7 +18,6 @@ module.exports = {
       }
     }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
